e7c857c2ce43da56f3199fcd7312b783771e2d44
ahonongobi <abyssiniea@gmaill.com>
2023-03-08 19:53:23 +0100

    capture ip 2

--- visitor.php
+++ visitor.php
@@ -1,20 +1,27 @@
+
+
 <?php
 // Get the user's IP address
 $ip_address = $_SERVER['REMOTE_ADDR'];
 
 // Use an API to get the user's country based on their IP address
 $country_api_url = 'https://api.ipgeolocation.io/ipgeo?apiKey=6c73195cfa794293afa977af0b904877&ip=' . $ip_address;
-$country_json = file_get_contents($country_api_url);
-$country_data = json_decode($country_json, true);
-$country = $country_data['country_name'];
+$country_json = file_get_contents($country_api_url) ?? '';
+$country_data = json_decode($country_json, true) ?? [];
+$country = $country_data['country_name'] ?? 'Unknown';
 
-// Send an email with the user's IP address and country
-$to = 'abyssiniea@gmail.com';
-$subject = 'User IP and country';
-$message = "User IP address: $ip_address\nUser country: $country";
-$headers = 'From: abyssiniea@gmail.com' . "\r\n" .
-           'Reply-To: abyssiniea@gmail.com' . "\r\n" .
-           'X-Mailer: PHP/' . phpversion();
+// Create a JSON object with the user's IP address and country
+$user_info = array(
+    'ip_address' => $ip_address,
+    'country' => $country
+);
+$user_info_json = json_encode($user_info);
 
-mail($to, $subject, $message, $headers);
+// Save the JSON object to a file
+$file_name = 'user_info.json';
+$file_handle = fopen($file_name, 'w');
+fwrite($file_handle, $user_info_json);
+fclose($file_handle);
 ?>
+
+
